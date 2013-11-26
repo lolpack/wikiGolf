1e07e5a146816e4777c2c5927f0825aae3d3a6c2
Aaron Pollack <pollackaaron@gmail.com>
2013-11-25 20:43:59 -0800

    Removed unused index file. Making fix for async facebook init call

--- static/js/models/user.js
+++ static/js/models/user.js
@@ -12,12 +12,16 @@ APP.UserModel = Backbone.Model.extend({
 		url: "/user"
 });
 
+
+
 APP.createUser = function () {
-  APP.insertUser = new APP.UserModel;
-  APP.insertUser.create({facebookPointer: APP.fbInfo.id, name : APP.fbInfo.name})
+	FB.api('/me', function(response) {
+			APP.fbInfo = {id: response.id, name : response.name};
+	};
+	APP.insertUser = new APP.UserModel;
+	APP.insertUser.create({facebookPointer: APP.fbInfo.id, name : APP.fbInfo.name})
 
-}
-
+};
 //The model should hold basic data about the user's identity (grabbed from facebook). And also his/her gamer stats.
 //This is relates to the course model as it will need to know if the player succesfully made it from the begging to the end 
 //Also, in order to calculate strokes, it will need to know how many pages the user went through. 
